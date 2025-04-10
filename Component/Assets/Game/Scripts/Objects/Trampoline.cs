b4fe9db3b32fa272a1fb7be2c093579b16b56ddb
Beshamon <141951306+BESHAM0N@users.noreply.github.com>
2025-04-10 21:01:55 +0300

    new refactoring

--- Component/Assets/Game/Scripts/Objects/Trampoline.cs
+++ Component/Assets/Game/Scripts/Objects/Trampoline.cs
@@ -18,8 +18,7 @@ namespace Component
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
-        {
-            Debug.Log("ПРЫГ");
+        {           
             _dropOffComponent.ExecuteDropOff();
         }
 
